e44f2a8c601a3b8775e7874ed8dd8879305f67ef
shekar <g.rajashekarreddy123@gmail.com>
2026-04-03 12:02:09 +0530

    Add ClientApp-OtherWay test file

---
+++ tests/ClientApp-OtherWay.spec.js
@@ -0,0 +1,45 @@
+
+const {test, expect} = require('@playwright/test');
+const console = require('node:console');
+
+test('Client App login', async ({ page }) => {
+    const userName=page.getByPlaceholder("email@example.com");
+    const password=page.getByPlaceholder("enter your passsword");
+    const productsName="ADIDAS ORIGINAL";
+    const products=page.locator(".card-body");
+   await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+   await userName.fill("anshika@gmail.com");
+   await password.fill("Iamking@000");
+   await page.getByRole("button", { name: "Login" }).click();
+   //wait till first product loads
+   await products.first().waitFor();
+   const titles=await page.locator(".card-body b").allTextContents();
+   const count=titles.length;
+   console.log(count);
+   console.log(titles);
+
+
+
+    //instead of above for loop code we can also use filter method to find the product and then click on add to cart button(in this single line)
+    await products.filter({hasText:productsName}).getByRole("button", { name: "Add To Cart" }).click();
+
+    //here clicking on cart ,but before that cart is in listitems so we have to first go to list item and then click on cart button
+     await page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+
+
+    //here waitFor is used because isVisible doesn't have auto wait
+    await page.locator("div li").first().waitFor();
+    //locator based on text and tag
+    const bool=await page.locator(`h3:has-text("${productsName}")`).isVisible();
+    await expect(page.getByText(productsName)).toBeVisible();
+
+
+    
+    await page.getByRole("button", { name: "Checkout" }).click();
+//here pressSequentially is used to type letter by letter with delay of 150ms because the dropdown is dynamic and it will show options after typing some letters
+    await page.getByPlaceholder("Select Country").pressSequentially("ind",{delay:150});
+    await page.getByRole("button", { name: "India" }).nth(1).click();
+    await page.getByText("Place Order").click();
+    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+
+});
