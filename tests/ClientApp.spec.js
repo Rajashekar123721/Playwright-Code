5886d1c1602edbd882f09d341adbee4e9674c042
shekar <g.rajashekarreddy123@gmail.com>
2026-04-03 11:05:35 +0530

    Initial commit

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,77 @@
+
+const {test, expect} = require('@playwright/test');
+const console = require('node:console');
+
+test('Client App login', async ({ page }) => {
+    const userName=page.locator("#userEmail");
+    const password=page.locator("#userPassword");
+    const productsName="ADIDAS ORIGINAL";
+    const products=page.locator(".card-body");
+   await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+   await userName.fill("anshika@gmail.com");
+   await password.fill("Iamking@000");
+   await page.locator("#login").click();
+   //wait till first product loads
+   await products.first().waitFor();
+   const titles=await page.locator(".card-body b").allTextContents();
+   const count=titles.length;
+   console.log(count);
+   console.log(titles);
+ 
+   const count1=await products.count();
+    for(let i=0;i<count1;i++){
+        if(await products.nth(i).locator("b").textContent()===productsName){    
+            //locator based on text add to cart
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+    }
+    await page.locator("[routerlink*='cart']").click();
+    //here waitFor is used because isVisible doesn't have auto wait
+    await page.locator("div li").first().waitFor();
+    //locator based on text and tag
+    const bool=await page.locator(`h3:has-text("${productsName}")`).isVisible();
+    expect(bool).toBeTruthy();
+
+
+    const checkout=page.locator("text=Checkout");
+    await checkout.click();
+//here pressSequentially is used to type letter by letter with delay of 150ms because the dropdown is dynamic and it will show options after typing some letters
+    await page.locator("[placeholder*='Country']").pressSequentially("ind",{delay:150});
+    const dropdown=page.locator(".ta-results");
+    await dropdown.waitFor();
+    const optionsCount=await dropdown.locator("button").count();
+    for(let i=0;i<optionsCount;i++){
+        const text=await dropdown.locator("button").nth(i).textContent();
+        if(text.trim()==="India"){
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+    await page.locator(".action__submit").click();
+    const orderConfirmation = await page.locator(".hero-primary").textContent();
+    expect(orderConfirmation).toContain("Thankyou for the order.");
+
+    const orderId=await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderId);
+
+    await page.locator("button[routerlink*='myorders']").click();
+    //wait till order items load
+    await page.locator("tbody").waitFor();
+//here we are getting all the rows of the table and then iterating through them to find the order id and then clicking on view button of that order id
+    const rows=await page.locator("tbody tr");
+    const rowCount=await rows.count();
+    for(let i=0;i<rowCount;i++){
+        const rowOrderId=await rows.nth(i).locator("th").textContent();
+        if(orderId.includes(rowOrderId)){
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    //checking order id
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    await page.pause();
+
+
+});
---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,89 @@
+
+const {test, expect} = require('@playwright/test');
+const console = require('node:console');
+
+test('Browser Context playwright test', async ({ browser }) => {
+   
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName=page.locator("input#username");
+    const signIn=page.locator("input#signInBtn");
+    const cardTitles=page.locator(".card-body a");
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    console.log(await page.title())
+    await userName.fill("rahulshetty");
+    await page.locator("[type='password']").fill("Learning@830$3mK2");
+    await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+        await userName.fill("");
+        await userName.fill("rahulshettyacademy");
+        await signIn.click();
+        console.log(await cardTitles.first().textContent());
+      //  console.log(await cardTitles.nth(1).textContent());
+        const allTitles=await cardTitles.allTextContents();
+        console.log(allTitles);
+
+});
+
+
+
+test('UI Controlls', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const userName=page.locator("input#username")
+    const signIn=page.locator("input#signInBtn");
+    const dropdown=page.locator("select.form-control");
+    const documentLink=page.locator("a[href*='documents-request']");
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    //2 types to check whether checkbox is checked or notw
+    console.log(await page.locator(".radiotextsty").last().isChecked());       //return true or false
+    await expect(await page.locator(".radiotextsty").last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    //there are 2 types to check whether checkbox is unchecked or not
+    await expect(page.locator("#terms")).not.toBeChecked();
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();   //hence it returns false we used falsy
+    
+    await expect(documentLink).toHaveAttribute("class","blinkingText");
+    
+    
+    //await page.pause();
+    
+});
+
+
+test.only('Child window handle', async ({ browser }) => {
+
+    //new Context is used to isolate the browser session
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const documentLink=page.locator("a[href*='documents-request']");
+
+    //handle to child window
+    //Promise.all is used to run 2 tasks simultaneously
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        documentLink.click(),
+
+    ]);
+    const text=await newPage.locator(".red").textContent();
+    const domain=text.split("@")[1].split(" ")[0];
+    //console.log(domain);
+
+    //to shift back to parent window
+    await page.bringToFront();
+    const userName=page.locator("input#username");
+    await userName.fill(domain);
+    await page.pause();
+    console.log(await userName.inputValue()); //will return empty because we are passing the text at run time instead we need to use inputValue()
+    //await page.pause();
+
+//close the pages
+    await newPage.close();
+    await page.close();
+
+});
